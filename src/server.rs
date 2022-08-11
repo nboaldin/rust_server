f613ae91a53ff63d18810e7d8a07b54b8f6d1c45
Nathan Boaldin <nathan.boaldin@banno.com>
2022-08-10 20:28:57 -0500

    more stuffs

--- src/server.rs
+++ src/server.rs
@@ -1,4 +1,6 @@
 use std::net::TcpListener;
+use std::convert::TryFrom;
+use crate::http::Request;
 use std::io::Read;
 
 pub struct Server {
@@ -22,7 +24,11 @@ impl Server {
                     let mut buffer = [0; 1024];
                     match stream.read(&mut buffer) {
                         Ok(_) => {
-                            println!("Recieved a request: {}", String::from_utf8_lossy(&buffer))
+                            println!("Recieved a request: {}", String::from_utf8_lossy(&buffer));
+                            match Request::try_from(&buffer[..]) {
+                                Ok(request) => {},
+                                Err(e) => print!("Failed to parse a request: {}",e),
+                            }
                         },
                         Err(e) => println!("Failed to read from connection: {}", e),
                     }
